de112aabb82d22d33dd0bfaa022c78e3b3ff5714
kiberd <kiberd22@gmail.com>
2022-09-20 16:04:31 +0900

    람다 함수 구현

--- bin/cdk-lambda-mlbstat.ts
+++ bin/cdk-lambda-mlbstat.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core';
 import { CdkLambdaMlbstatStack } from '../lib/cdk-lambda-mlbstat-stack';
 
 const app = new cdk.App();
@@ -11,11 +11,11 @@ new CdkLambdaMlbstatStack(app, 'CdkLambdaMlbstatStack', {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  // env: { account: '670740772710', region: 'us-west-2' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/cdk-lambda-mlbstat-stack.ts
+++ lib/cdk-lambda-mlbstat-stack.ts
@@ -1,16 +1,123 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+
+import * as cdk from "@aws-cdk/core";
+import * as ssm from "@aws-cdk/aws-ssm";
+import * as lambda from "@aws-cdk/aws-lambda";
+
+const apigateway = require("@aws-cdk/aws-apigateway");
 
 export class CdkLambdaMlbstatStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a lambda function, associate with js file
+    const getBattingStats = new lambda.Function(this, "getBattingStatsLambda", {
+      code: new lambda.AssetCode("src"),
+      handler: "battingStats.getBattingStats",
+      runtime: lambda.Runtime.NODEJS_16_X,
+      timeout: cdk.Duration.seconds(100),
+      environment: {
+        ELASTICSEARCH_IP: ssm.StringParameter.valueFromLookup(
+          this,
+          "ELASTIC_BASEBALL_IP"
+        ),
+        ELASTICSEARCH_USERNAME: ssm.StringParameter.valueFromLookup(
+          this,
+          "ELASTIC_BASEBALL_USERNAME"
+        ),
+        ELASTICSEARCH_PASSWORD: ssm.StringParameter.valueFromLookup(
+          this,
+          "ELASTIC_BASEBALL_PASSWORD"
+        ),
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkLambdaMlbstatQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create a lambda function, associate with js file
+    const searchPlayers = new lambda.Function(this, "searchPlayers", {
+      code: new lambda.AssetCode("src"),
+      handler: "search.searchPlayers",
+      runtime: lambda.Runtime.NODEJS_16_X,
+      timeout: cdk.Duration.seconds(100),
+      environment: {
+        ELASTICSEARCH_IP: ssm.StringParameter.valueFromLookup(
+          this,
+          "ELASTIC_BASEBALL_IP"
+        ),
+        ELASTICSEARCH_USERNAME: ssm.StringParameter.valueFromLookup(
+          this,
+          "ELASTIC_BASEBALL_USERNAME"
+        ),
+        ELASTICSEARCH_PASSWORD: ssm.StringParameter.valueFromLookup(
+          this,
+          "ELASTIC_BASEBALL_PASSWORD"
+        ),
+      },
+    });
+
+    // Create API Gateway resource
+    const api = new apigateway.RestApi(this, "baseballAPI", {
+      restApiName: "Baseball Stats REST API",
+    });
+
+    // Set up /batting endpoint
+    const batting = api.root.addResource("batting");
+    const getBatting = batting.addResource("{playerId}");
+    addCorsOptions(batting);
+    addCorsOptions(getBatting);
+
+    // Set up /search endpoint
+    const search = api.root.addResource("search");
+    addCorsOptions(search);
+
+    // Set up apiGateway/lambda integrations
+    const getBattingIntegration = new apigateway.LambdaIntegration(
+      getBattingStats
+    );
+    const searchPlayersIntegration = new apigateway.LambdaIntegration(
+      searchPlayers
+    );
+
+    // Associate each integration with HTTP verb
+    getBatting.addMethod("GET", getBattingIntegration);
+    search.addMethod("POST", searchPlayersIntegration);
   }
+}
+
+export function addCorsOptions(apiResource: any) {
+  apiResource.addMethod(
+    "OPTIONS",
+    new apigateway.MockIntegration({
+      integrationResponses: [
+        {
+          statusCode: "200",
+          responseParameters: {
+            "method.response.header.Access-Control-Allow-Headers":
+              "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
+            "method.response.header.Access-Control-Allow-Origin": "'*'",
+            "method.response.header.Access-Control-Allow-Credentials":
+              "'false'",
+            "method.response.header.Access-Control-Allow-Methods":
+              "'OPTIONS,GET,PUT,POST,DELETE'",
+          },
+        },
+      ],
+      passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+      requestTemplates: {
+        "application/json": '{"statusCode": 200}',
+      },
+    }),
+    {
+      methodResponses: [
+        {
+          statusCode: "200",
+          responseParameters: {
+            "method.response.header.Access-Control-Allow-Headers": true,
+            "method.response.header.Access-Control-Allow-Methods": true,
+            "method.response.header.Access-Control-Allow-Credentials": true,
+            "method.response.header.Access-Control-Allow-Origin": true,
+          },
+        },
+      ],
+    }
+  );
 }
